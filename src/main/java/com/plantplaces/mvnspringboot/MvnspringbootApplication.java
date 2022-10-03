d62454fce2412a7cc302665f4189371944cc1234
EdgySantana <tesseus2006@gmail.com>
2022-10-03 15:55:53 -0400

    First commit

---
+++ src/main/java/com/plantplaces/mvnspringboot/MvnspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.plantplaces.mvnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnspringbootApplication.class, args);
+	}
+
+}
